9144b97a01cee3d8e8e77bba6d113eb53e773b94
narayanan1208 <narayanan6647@gmail.com>
2025-03-16 14:30:31 +0530

    Added db connection file

---
+++ backend/config/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const mongoDB_URI = process.env.MONGODB_URI;
+
+export const connectDB = async () => {
+  await mongoose.connect(mongoDB_URI).then(() => console.log("DB connected"));
+};
