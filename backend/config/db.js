a7ebd3b2480de53dde8736a117f5f40150be3e6e
narayanan1208 <narayanan6647@gmail.com>
2025-03-16 18:22:20 +0530

    Updated db connection file

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,5 +6,8 @@ dotenv.config();
 const mongoDB_URI = process.env.MONGODB_URI;
 
 export const connectDB = async () => {
-  await mongoose.connect(mongoDB_URI).then(() => console.log("DB connected"));
+  await mongoose
+    .connect(mongoDB_URI)
+    .then(() => console.log("DB connected"))
+    .catch((err) => console.error("MongoDB Connection Error:", err));
 };
