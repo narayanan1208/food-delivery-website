7bc9862fdfe82a21afff1032f3cd5ddfa4022ff8
narayanan1208 <narayanan6647@gmail.com>
2025-03-27 14:15:41 +0530

    Updated authentication file

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-const authMiddleware = async (req, resizeBy, next) => {
+const authMiddleware = async (req, res, next) => {
   const { token } = req.headers;
   if (!token) {
     return res.json({ success: false, message: "not Authorized Login Again" });
