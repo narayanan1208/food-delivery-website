81e7365f5af743860978f28133c2e5e8150b6895
narayanan1208 <narayanan6647@gmail.com>
2025-04-01 00:29:40 +0530

    Added status handler route in frontend

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -46,7 +46,7 @@ const MyOrders = () => {
                 <span>&#x25cf;</span>
                 <b>{order.status}</b>
               </p>
-              <button>Track order</button>
+              <button onClick={fetchOrders}>Track order</button>
             </div>
           );
         })}
