6c7e2f17e745365c1bbcb285e83a277cea074156
narayanan1208 <narayanan6647@gmail.com>
2025-04-01 00:29:15 +0530

    Added status handler route in admin

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -8,12 +8,22 @@ const Orders = ({ url }) => {
   const [orders, setOrders] = useState([]);
 
   const fetchAllOrders = async () => {
-    const response = await axios.get(url + "/api/order/list");
+    const response = await axios.post(url + "/api/order/list");
     if (response.data.success) {
       setOrders(response.data.data);
       console.log(response.data.data);
     } else {
       toast.error("Error");
+    }
+  };
+
+  const statusHandler = async (event, orderId) => {
+    const response = await axios.post(url + "/api/order/status", {
+      orderId: orderId,
+      status: event.target.value,
+    });
+    if (response.data.success) {
+      await fetchAllOrders();
     }
   };
 
@@ -57,7 +67,12 @@ const Orders = ({ url }) => {
             </div>
             <p>Items: {order.items.length}</p>
             <p>${order.amount}</p>
-            <select name="" id="">
+            <select
+              onChange={(event) => statusHandler(event, order._id)}
+              value={order.status}
+              name=""
+              id=""
+            >
               <option value="Food Processing">Food Processing</option>
               <option value="Out for Delivery">Out for Delivery</option>
               <option value="Delivered">Delivered</option>
